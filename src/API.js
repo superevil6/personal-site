341a5c2832d2d4ee1340ae6a1c7d4624a01642b8
superevil6 <apex.xp@gmail.com>
2020-03-16 15:22:26 -0500

    updating for cors

--- src/API.js
+++ src/API.js
@@ -12,7 +12,7 @@ app.use(
     })
 )
 app.use( (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "alexcassells.com");
+    res.header("Access-Control-Allow-Origin", "https://alexcassells.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
